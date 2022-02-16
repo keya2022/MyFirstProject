8665b1d7130e6a30d4cd7ef7eabf571e0b3010e2
keya2022 <99786348+keya2022@users.noreply.github.com>
2022-02-16 12:08:50 +0530

    program of addition
    succesfully done

---
+++ Pro2.java
@@ -0,0 +1,12 @@
+class Pro2{
+    public static void main(String args[])
+    {
+        System.out.println("keya");
+        int a=5; 
+        int b=6;
+        int c=a+b;
+        System.out.println("result"+c);
+        
+    }
+
+}
